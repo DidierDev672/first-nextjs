d05cbf1d15c461ba8f16ee4854eecec2fb746b3e
DidierDev672 <ariasdev672@gmail.com>
2025-12-28 20:38:44 -0500

    Components para ganaderia estelar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import AcmeLogo from "@/app/ui/acme-logo";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
-import styles from "@/app/ui/home.module.css";
-import { lusitana, quintessential } from "./ui/fonts";
+import { quintessential } from "./ui/fonts";
 import Image from "next/image";
+import "@radix-ui/themes/styles.css";
 
 export default function Page() {
   return (
